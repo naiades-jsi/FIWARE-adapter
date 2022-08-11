405f4a79fdcb63e71cdf338da3697afc762214a1
Gal petkovšek <gp1914@student.uni-lj.si>
2022-08-11 11:16:43 +0200

    from field corrected

--- FIWARE-adapter/from_corrector2.py
+++ FIWARE-adapter/from_corrector2.py
@@ -10,7 +10,7 @@ for conf_client in conf_clients:
     with open("config/" + conf_client, "r") as data_file_old:
         print(conf_client)
         conf_old = json.load(data_file_old)
-        conf_old["from"] = "2022-08-05T21:30:00.000"
+        conf_old["from"] = "2022-08-09T21:30:00.000"
     
     with open("config/" + conf_client, "w") as wrt:
         json.dump(conf_old, wrt)
