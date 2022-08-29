3c8e37274e157317c77fb32fed8d937f84595721
Gal petkovšek <gp1914@student.uni-lj.si>
2022-08-29 14:14:45 +0200

    SIAMVI configuration

--- FIWARE-adapter/from_corrector.py
+++ FIWARE-adapter/from_corrector.py
@@ -7,11 +7,13 @@ conf_clients = conf["clients"]
 
 for conf_client in conf_clients:
     path = "config/" + conf_client
-    file_name = conf_client.split("/")[1]
-
+    use_case = conf_client.split("/")[1]
+    file_name = conf_client.split("/")[2]
+    print(file_name)
     with open(path) as data_file_old:
-        with open("config/productionSIMAVI/" + file_name, "r+") as data_file_new:
+        with open("config/productionSIMAVI/" + use_case + "/" + file_name, "r") as data_file_new:
             conf_new = json.load(data_file_new)
             conf_old = json.load(data_file_old)
+        with open("config/productionSIMAVI/" + use_case + "/" + file_name, "w") as data_file_new_r:
             conf_new["from"] = conf_old["from"]
-            json.dump(conf_new, data_file_new)
+            json.dump(conf_new, data_file_new_r)
